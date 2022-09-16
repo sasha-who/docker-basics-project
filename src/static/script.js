1ece3ea4753397f42c3b844f36b521c9c3a5e632
sasha-who <warmsasha@gmail.com>
2022-09-16 10:48:08 +0300

    Add a submit button to save a new note

--- src/static/script.js
+++ src/static/script.js
@@ -1,13 +1,39 @@
+const createNoteElement = (note) => {
+  const notesContainerElement = document.querySelector(".notes-container");
+  const newNoteElement = document.createElement("p");
+
+  newNoteElement.append(note.text);
+  notesContainerElement.append(newNoteElement);
+};
+
 (async () => {
   // Fetch existing notes and mount them on page
   const response = await fetch("/notes");
   const notes = await response.json();
-  const notesContainerElement = document.querySelector(".notes-container");
 
   for (const note of notes) {
-    const newNoteElement = document.createElement("p");
-
-    newNoteElement.append(note.text);
-    notesContainerElement.append(newNoteElement);
+    createNoteElement(note);
   }
+
+  // Send a new note to a server
+  const addButtonElement = document.getElementById("add-button");
+  const inputElement = document.getElementById("text-input");
+
+  addButtonElement.addEventListener("click", (async () => {
+    const note = { text: inputElement.value };
+    inputElement.value = "";
+
+    const response = await fetch("/note", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(note)
+    })
+
+    if (response) {
+      createNoteElement(note)
+    }    
+  }));
 })();
